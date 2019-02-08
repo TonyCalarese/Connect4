fc037f868eb51fd55dff7727e8f4969dfb91bafc
Tony <35467453+TonyCalarese@users.noreply.github.com>
2019-02-07 23:36:48 -0500

    psuedo code for horizontal and downwards
    The code is iffy and will really only be able to work if the i and j values do not fully increment so we will have to figure out how to make it work better.

--- util.go
+++ util.go
@@ -1,19 +1,29 @@
 package main
 
 // CheckHorizontal checks if there is a winning horizontal segment
-func CheckHorizontal(board C4Board) (win bool, segments []Segment) {
-
-	return
+func (board C4Board) checkHorizontal() bool {	
+	if board.position[i][j] && board.position[i+1][j] && board.position[i+2][j] == board.position[i+3][j] {
+	return true
+	}
+	else {
+	return false
+	}
 }
 
 // CheckVertical checks if there is a winning vertical segment
-func CheckVertical() (win bool, segments []Segment) {
-
-	return
+func (board C4Board) checkVerticle bool {
+	if board.position[i][j] && board.position[i][j+1] && board.position[i][j+2] == board.position[i][j+3] {
+	return true
+	}
+	else {
+	return false
+	}
 }
 
 // CheckDiagonal checks if there is a winning diagonal segment
-func CheckDiagonal(board C4Board) (win bool, segments []Segment) {
-
+func (board C4Board) checkDiagnal() bool {
+		
+	//Check left to right
+	//Check right to left
 	return
 }
