0965fb254b174a0fdb1fcef082ebcdea1b1260ae
Adam DeCosta <adam@decosta.io>
2019-02-09 00:19:01 -0500

    comment updates

--- connect4.go
+++ connect4.go
@@ -71,25 +71,9 @@ func (board C4Board) LegalMoves() []Move {
 }
 
 // IsWin calculates if the board is in a winning position
+// if it is, then returns true, else returns false.
 func (board C4Board) IsWin() bool {
-	/*
-		Board:
-		|_|_|_|_|_|_|_|
-		|_|_|_|_|_|_|_|
-		|_|_|_|_|_|_|_|
-		|_|_|_|_|_|_|_|
-		|_|_|_|_|_|_|_|
-		|_|_|_|_|_|_|_|
 
-
-		|(0,5)|(1,5)|(2,5)|(3,5)|(4,5)|(5,5)|(6,5)|
-		|(0,4)|(1,4)|(2,4)|(3,4)|(4,4)|(5,4)|(6,4)|
-		|(0,3)|(1,3)|(2,3)|(3,3)|(4,3)|(5,3)|(6,3)|
-		|(0,2)|(1,2)|(2,2)|(3,2)|(4,2)|(5,2)|(6,2)|
-		|(0,1)|(1,1)|(2,1)|(3,1)|(4,1)|(5,1)|(6,1)|
-		|(0,0)|(1,0)|(2,0)|(3,0)|(4,0)|(5,0)|(6,0)|
-
-	*/
 	_, horizontalWin := board.CheckHorizontal()
 	_, verticalWin := board.CheckVertical()
 	_, diagonalWin := board.CheckDiagonal()
