9078ffe3b73431e61e3625d52c2da5ab633b03ab
Tony <35467453+TonyCalarese@users.noreply.github.com>
2019-02-12 21:52:28 -0500

    Tried Tackling Concurrency, not done
    Tried to tackle concurrency and I am going to need to tackle this more tomorrow and with Kopec as well

--- play.go
+++ play.go
@@ -63,7 +63,7 @@ func main() {
 		}
 		gameBoard = gameBoard.MakeMove(col)
 		gameBoard = gameBoard.MakeMove(FindBestMove(gameBoard, 3))
-		//gameBoard = gameBoard.MakeMove(go FindBestMove(gameBoard, 3))
+		//gameBoard = gameBoard.MakeMove(go ConcurrentFindBestMove(gameBoard, 3))
 		fmt.Printf("Black: %f", gameBoard.Evaluate(Black))
 		fmt.Printf("Red: %f", gameBoard.Evaluate(Red))
 		fmt.Println()
